2d8cb047a8b2350df44bfc55b79bae4576fe781f
minh <hoanggminh@gmail.com>
2022-10-16 22:54:31 +0200

    fix output display

--- homeworks/homework_3/guessing_game/guessing_game.cpp
+++ homeworks/homework_3/guessing_game/guessing_game.cpp
@@ -9,9 +9,9 @@ int main () {
     int num_min, num_max;
     cout << "Welcome to the GUESSING GAME!" << endl;
     cout << "I will generate a number and you will guess it!" << endl;
-    cout << "Please provide the smallest number:" << endl;
+    cout << "Please provide the smallest number: " << endl;
     cin >> num_min;
-    cout << "Please provide the largest number:" << endl;
+    cout << "Please provide the largest number: " << endl;
     cin >> num_max;
 
     std::random_device random_device;
@@ -24,10 +24,10 @@ int main () {
     int count = 0;
     while (1) {
         count++;
-        cout << "Please provide the next guess:" << endl;
+        cout << "Please provide the next guess: ";
         cin >> num_in;
         if (num_in == num_gen) {
-            cout << "You've done it! You guessed the number " << num_gen << " in " << count << " guesses!" << endl;
+            cout << "You've done it! You guessed the number " << num_gen << " in " << count << " guesses!";
             break;
         } else if (num_in < num_gen) {
             cout << "Your number is too small. Try again!" << endl;
